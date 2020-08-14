668930df122a537edf6f63ce66dc3c8ff0a1a58d
Joshua Weisbrod-Torres <64339271+joshuaweisbrodtorres@users.noreply.github.com>
2020-08-13 22:38:38 -0500

    fixed user delete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -121,22 +121,30 @@ router.put('/:id', (req,res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req,res) => {
-    User.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(dbUserData => {
-        if (!dbUserData) {
-            res.status(404).json({ message: 'No user found with this id'})
-        }
-        res.json(dbUserData)
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+	Comment.destroy({
+		where : {
+			user_id : req.params.id
+		}
+	}).then(() => {
+		User.destroy({
+			where : {
+				id : req.params.id
+			}
+		})
+			.then((dbUserData) => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'No user found with this id' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch((err) => {
+				console.log(err);
+				res.status(500).json(err);
+			});
+	});
 });
 
 module.exports = router;
