f12ac4cfa04af6a9da82242820d6b1efb8aa3b1b
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-18 16:45:11 -0500

    edit and delete funcationality work

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,7 +1,13 @@
 async function editFormHandler(event) {
     event.preventDefault();
 
-    const response = await fetch(`/api/post/${id}`, {
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ]
+
+    const title = document.querySelector('input[name="post-title"]').value;
+
+    const response = await fetch(`/api/posts/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             title
