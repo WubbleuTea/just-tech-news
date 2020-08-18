a16c86e636e9a4bbfb5e1080340aaddf9da1a7a4
Joshua Weisbrod-Torres <jwtorres@rocketmail.com>
2020-08-17 22:38:33 -0500

    added tests for helpers and implemented helpers

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
+const helpers = require('./utils/helpers');
 // starts the express connection
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
 // import handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store)
 // creates the session
